a2d19622f6c6d405ba3db70cb7e78890524d77a6
brendon3421 <brendonrroger@gmail.com>
2024-08-27 22:55:44 -0300

    Alteracao no css e adicionado o Sweather

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-
 function limpa_formulário_cep() {
-    //Limpa valores do formulário de cep.
     document.getElementById('rua').value = ("");
     document.getElementById('bairro').value = ("");
     document.getElementById('cidade').value = ("");
     document.getElementById('uf').value = ("");
     document.getElementById('ibge').value = ("");
-    document.getElementById('ddd').value = ("")
-    document.getElementById('gia').value = ("")
+    document.getElementById('ddd').value = ("");
+    document.getElementById('gia').value = ("");
 }
 
 function meu_callback(conteudo) {
     if (!("erro" in conteudo)) {
-        //Atualiza os campos com os valores.
         document.getElementById('rua').value = (conteudo.logradouro);
         document.getElementById('bairro').value = (conteudo.bairro);
         document.getElementById('cidade').value = (conteudo.localidade);
@@ -20,29 +17,24 @@ function meu_callback(conteudo) {
         document.getElementById('ibge').value = (conteudo.ibge);
         document.getElementById('ddd').value = (conteudo.ddd);
         document.getElementById('gia').value = (conteudo.gia);
-    } //end if.
-    else {
-        //CEP não Encontrado.
+    } else {
         limpa_formulário_cep();
-        alert("CEP não encontrado.");
+        Swal.fire({
+            title: 'Erro',
+            text: 'CEP não encontrado.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
     }
 }
 
 function pesquisacep(valor) {
-
-    //Nova variável "cep" somente com dígitos.
     var cep = valor.replace(/\D/g, '');
 
-    //Verifica se campo cep possui valor informado.
     if (cep != "") {
-
-        //Expressão regular para validar o CEP.
         var validacep = /^[0-9]{8}$/;
 
-        //Valida o formato do CEP.
         if (validacep.test(cep)) {
-
-            //Preenche os campos com "..." enquanto consulta webservice.
             document.getElementById('rua').value = "...";
             document.getElementById('bairro').value = "...";
             document.getElementById('cidade').value = "...";
@@ -50,27 +42,24 @@ function pesquisacep(valor) {
             document.getElementById('ibge').value = "...";
             document.getElementById('ddd').value = "...";
             document.getElementById('gia').value = "...";
-            //Cria um elemento javascript.
+
             var script = document.createElement('script');
-
-            //Sincroniza com o callback.
             script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
-
-            //Insere script no documento e carrega o conteúdo.
             document.body.appendChild(script);
 
-        } //end if.
-        else {
-            //cep é inválido.
+        } else {
             limpa_formulário_cep();
-            alert("Formato de CEP inválido.");
+            Swal.fire({
+                title: 'Erro',
+                text: 'Formato de CEP inválido.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
         }
-    } //end if.
-    else {
-        //cep sem valor, limpa formulário.
+    } else {
         limpa_formulário_cep();
     }
-};
+}
 
 let longitudeInput = document.getElementById("longitude");
 let latitudeInput = document.getElementById("Latitude");
@@ -80,14 +69,11 @@ var map;
 function success(pos) {
     console.log(pos.coords.latitude, pos.coords.longitude);
 
-    // Atualizar os valores dos elementos de entrada
     latitudeInput.value = pos.coords.latitude;
     longitudeInput.value = pos.coords.longitude;
 
-    // Atualizar o conteúdo do h2
     h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
 
-    // Mostrar o mapa
     if (map === undefined) {
         map = L.map("map").setView([pos.coords.latitude, pos.coords.longitude], 13);
     } else {
@@ -95,7 +81,6 @@ function success(pos) {
         map = L.map("map").setView([pos.coords.latitude, pos.coords.longitude], 13);
     }
 
-    // Mostrar as ruas/prédios e bairros
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
@@ -111,8 +96,6 @@ function success(pos) {
 
     map.on('click', onMapClick);
 
-
-    // Adicionar marcador
     L.marker([pos.coords.latitude, pos.coords.longitude])
         .addTo(map)
         .bindPopup("Eu estou neste local!")
@@ -128,8 +111,22 @@ var watchID = navigator.geolocation.watchPosition(success, erro, {
     timeout: 1000,
 });
 
-//mascara
 var cepInput = document.getElementById('cep');
 var maskOptionsCpf = {
-    mask: '00000-000' // Define a máscara como ###.###.###-##
+    mask: '00000-000'
 };
+
+
+function displayUserIP() {
+    axios.get('https://api.ipify.org?format=json')
+        .then(response => {
+            document.getElementById('user-ip').textContent = `Seu IP é: ${response.data.ip}`;
+        })
+        .catch(error => {
+            console.error('Erro ao obter IP do usuário:', error);
+            document.getElementById('user-ip').textContent = 'Não foi possível obter o IP.';
+        });
+}
+
+// Chama a função quando a página carrega
+window.onload = displayUserIP;
